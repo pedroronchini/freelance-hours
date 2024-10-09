1409b5dae32c60f8a1aa01046942ae7c1ba1e9d7
Pedro Henrique Ronchini <pe-ronchini@hotmail.com>
2024-10-09 11:50:04 -0300

    salvando proposta

--- resources/views/components/projects/card.blade.php
+++ resources/views/components/projects/card.blade.php
@@ -4,7 +4,8 @@
     <div class="flex items-start justify-between pb-4">
         <div class="flex flex-col gap-[16px]">
             <div>
-                <span class="bg-[#C0F7B4] text-[#1D8338] rounded-full font-bold text-center uppercase py-[6px] px-[14px] text-[12px] tracking-wide ">
+                <span
+                    class="bg-[#C0F7B4] text-[#1D8338] rounded-full font-bold text-center uppercase py-[6px] px-[14px] text-[12px] tracking-wide ">
                     {{ $project->status->label() }}
                 </span>
             </div>
@@ -16,12 +17,9 @@
             </div>
         </div>
         <div>
-            <button class="bg-[#5354FD] text-white font-bold tracking-wide uppercase px-8 py-3 rounded-[4px]
-                    hover:bg-[#1f20a6] transition duration-300 ease-in-out"
-                    @click="$refs.modal.showModal(); showModal= true"
-            >
-                Enviar uma proposta
-            </button>
+            <livewire:proposals.create :$project />
+
+
             <div class="flex items-center justify-between mt-4 text-[14px]">
                 <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
                 <div class="font-bold flex items-center space-x-1">
@@ -41,8 +39,8 @@
     <div class="py-4 space-y-4">
         <div class="uppercase font-bold text-[#8C8C9A] text-[12px]">Tecnologias</div>
         <div class="flex gap-[8px] items-center pb-2">
-            @foreach($project->tech_stack as $tech)
-                <x-ui.tech :icon="$tech" :text="$tech"/>
+            @foreach ($project->tech_stack as $tech)
+                <x-ui.tech :icon="$tech" :text="$tech" />
             @endforeach
         </div>
     </div>
@@ -51,7 +49,7 @@
         <div class="uppercase font-bold text-[#8C8C9A] text-[12px]">Publicado Por</div>
         <div class="flex gap-[8px] items-center">
             <div>
-                <x-ui.avatar src="{{ $project->author->avatar }}"/>
+                <x-ui.avatar src="{{ $project->author->avatar }}" />
             </div>
 
             <div>
@@ -59,8 +57,8 @@
                     {{ $project->author->name }}
                 </div>
                 <div class="flex items-center space-x-[4px]">
-                    @foreach(range(1, $project->author->stars) as $star)
-                        <x-ui.icons.star class="h-[14px]"/>
+                    @foreach (range(1, $project->author->stars) as $star)
+                        <x-ui.icons.star class="h-[14px]" />
                     @endforeach
                 </div>
             </div>
