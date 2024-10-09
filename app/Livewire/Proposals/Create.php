10f944d969bc6edfdfae0cfd8e952761c68a81fc
Pedro Henrique Ronchini <pe-ronchini@hotmail.com>
2024-10-09 17:07:33 -0300

    atualizando posições

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,10 +2,16 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
+use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+
 
 class Create extends Component
 {
@@ -13,7 +19,7 @@ class Create extends Component
 
     public bool $modal = false;
 
-    #[Rule(['required', 'email'])]
+    #[Validate(['required', 'email'])]
     public string $email = '';
 
     #[Rule(['required', 'numeric', 'gt:0'])]
@@ -25,19 +31,47 @@ class Create extends Component
     {
         $this->validate();
 
-        if (!$this->agree) {
-            $this->addError('agree', 'Você precisa concordadar com os termos de uso');
+        if (! $this->agree) {
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+
             return;
         }
 
-        $this->project->proposals()->updateOrCreate(
-            ['email' => $this->email],
-            ['hours' => $this->hours]
-        );
+        DB::transaction(function () {
+            $proposal = $this->project->proposals()
+                ->updateOrCreate(
+                    ['email' => $this->email],
+                    ['hours' => $this->hours]
+                );
+            $this->arrangePositions($proposal);
+        });
+
+        $this->project->author->notify(new NewProposal($this->project));
 
         $this->dispatch('proposal::created');
-
         $this->modal = false;
+    }
+
+    public function arrangePositions(Proposal $proposal)
+    {
+        $query = DB::select('
+            select *, row_number() over (order by hours asc) as newPosition
+            from proposals
+            where project_id = :project
+            ', ['project' => $proposal->project_id]);
+
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+
+        if ($otherProposal) {
+            $proposal->update(['position_status' => 'up']);
+            $oProposal = Proposal::find($otherProposal->id);
+
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
+        }
+
+        ArrangePositions::run($proposal->project_id);
     }
 
     public function render()
